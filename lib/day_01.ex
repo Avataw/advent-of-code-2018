bb3770328115d5234d7654c857a4b0e2e0956b4a
Avataw <avataw.dev@gmail.com>
2023-10-13 13:53:55 +0200

    add day 01

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -1,5 +1,28 @@
 defmodule Day01 do
-  def hello do
-    :world
+  def get_numbers(stream) do
+    stream
+    |> Stream.map(&ParseHelper.try_parse/1)
+  end
+
+  def solve_a do
+    FileHelper.read_file(1)
+    |> get_numbers
+    |> Enum.sum()
+  end
+
+  def sum_until_first_duplicate(x, acc) do
+    {frequency, set} = acc
+    if MapSet.member?(set, frequency) do
+      {:halt, frequency}
+    else
+      {:cont, {frequency + x, MapSet.put(set, frequency) }}
+    end
+  end
+
+  def solve_b do
+    FileHelper.read_file(1)
+    |> get_numbers
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, MapSet.new()}, &sum_until_first_duplicate/2)
   end
 end
--- test/day_01_test.exs
+++ test/day_01_test.exs
@@ -1,7 +1,12 @@
 defmodule Day01Test do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Day01.hello() == :world
+  test "solves a" do
+    assert Day01.solve_a() == 427
   end
+
+  test "solves b" do
+    assert Day01.solve_b() == 341
+  end
+
 end
